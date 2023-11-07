6b9ae9a006de07859e96a3e9ee59031ce12b76ae
manh.nguyen <manh.nguyen@192.168.40.22>
2023-11-07 20:19:33 +0700

    push

--- miniapp/MiniAppOne/App.tsx
+++ miniapp/MiniAppOne/App.tsx
@@ -1,37 +1,75 @@
 import React from 'react';
 import {
-  Text,
+  Dimensions,
+  NativeModules,
   SafeAreaView,
   StyleSheet,
+  Text,
   TouchableOpacity,
-  NativeModules,
+  View,
 } from 'react-native';
+import MapView, {Callout, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import AppInfo from './app.json';
 
+const {height} = Dimensions.get('window');
 const {ConnectNativeModule} = NativeModules;
-const App = (props: any): JSX.Element => {
+const App = props => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>App One</Text>
-      <Text style={styles.content}>
-        Here props from super app: {JSON.stringify(props)}
-      </Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          ConnectNativeModule?.closeApp(AppInfo.name);
-        }}>
-        <Text style={styles.content}>Close App</Text>
-      </TouchableOpacity>
+    <SafeAreaView>
+      <View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          zoomControlEnabled={true}
+          zoomEnabled={true}
+          style={{
+            height: height - 200,
+          }}
+          region={{
+            latitude: 16.069560532577032,
+            longitude: 108.23408695899637,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: 16.069560532577032,
+              longitude: 108.23408695899637,
+            }}>
+            <Callout>
+              <Text>Address</Text>
+            </Callout>
+          </Marker>
+        </MapView>
+      </View>
+      <View>
+        <Text style={styles.title}>App One</Text>
+        <Text style={styles.content}>
+          Here props from super app: {JSON.stringify(props)}
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            ConnectNativeModule?.closeApp(AppInfo.name);
+          }}>
+          <Text style={styles.content}>Close App</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ffffff',
-    flex: 1,
-  },
+  // container: {
+  //   marginTop: 10,
+  //   paddingVertical: 10,z
+  //   borderRadius: 5,
+  //   borderWidth: 1,
+  //   paddingHorizontal: 15,
+  //   justifyContent: 'center',
+  // },
+
   title: {
     fontSize: 24,
     color: 'red',
@@ -51,5 +89,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-export default App;
